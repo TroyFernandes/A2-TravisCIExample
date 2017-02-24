dc7828e34c7bf4543bb270ad12f54ee687abf2aa
troyfernandes <troyfernandes27@gmail.com>
2017-02-24 18:57:51 -0500

    changes

---
+++ Test/test2.js
@@ -0,0 +1,7 @@
+describe("First Test", function(){
+    it("Should pass the second test",function(){
+        var division = require('../main.js');
+        var data = [ 89165.19,1.358284,,12358,3197,6582,3180.2,19651,9841,3546,8135,1685,1351,681,3551 ];
+        testExample(data);
+    });
+});
